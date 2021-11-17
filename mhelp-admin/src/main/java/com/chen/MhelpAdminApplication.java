3018aaf87b53f097ea526e0141330c92a731a0ce
Rodma1 <1156028692@qq.com>
2021-11-17 15:01:14 +0800

    后台项目基本架构配置完成

---
+++ mhelp-admin/src/main/java/com/chen/MhelpAdminApplication.java
@@ -0,0 +1,13 @@
+package com.chen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhelpAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MhelpAdminApplication.class, args);
+    }
+
+}
