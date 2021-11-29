c8e9b00f1e81f3d0884b35d164caa595c122945d
Rodma1 <1156028692@qq.com>
2021-11-29 21:19:35 +0800

    实现了动态绑定路由（管理员端）

--- mhelp-vueadmin/mhelp-admin/src/router/index.js
+++ mhelp-vueadmin/mhelp-admin/src/router/index.js
@@ -59,6 +59,32 @@ router.beforeEach((to, from, next) => {
       store.commit("setMenuList", res.data.data.nav)
       //  拿到用户权限permList
       store.commit("setPermList", res.data.data.authoritys)
+      // console.log(store.state.menus.menuList)
+
+
+      //  我们已经从后端获取到路由path了，我们现在动态绑定他
+      let newRoutes = router.options.routes//定义路由管理器
+      //遍历每一个菜单
+      res.data.data.nav.forEach(menu => {
+        //  如果有子菜单也要遍历
+        if (menu.children) {
+          menu.children.forEach(m => {
+            //  转成路由
+            let route = menuToRoute(m)
+            //  如果路由route不为null
+            if (route) {
+              //  把路由添加到路由管理器中,Home
+              newRoutes[0].children.push(route)
+            }
+
+          })
+        }
+      })
+      // console.log(newRoutes)
+      // console.log("hh")
+    //  重新添加到上面的router中
+      router.addRoutes(newRoutes)
+    //  运行到这里说明我们已经遍历过一遍menus菜单了，将hasRoute改为true
       hasRoute=true
       store.commit("changeRouteStatus",hasRoute)
     })
@@ -66,4 +92,25 @@ router.beforeEach((to, from, next) => {
   next()
 })
 
+//导航转成路由
+const menuToRoute = (menu) => {
+  //如果没有导航，列："sys/User"。就返回空
+  if (!menu.component) {
+    return null;
+  }
+//  设定路由
+  let route = {
+    name: menu.name,
+    //路由
+    path: menu.path,
+    //参数
+    meta: {
+      icon: menu.icon,
+      title: menu.title
+    }
+  }
+//  路由组件
+  route.component = () => import('@/views/' + menu.component + '.vue')
+  return route
+}
 export default router
