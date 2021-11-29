64a0ce234a97265ad976c199cc9378f3ac4d8d99
Rodma1 <1156028692@qq.com>
2021-11-29 16:10:36 +0800

    将菜单栏分离出来完成（管理员端）

--- mhelp-vueadmin/mhelp-admin/src/views/Home.vue
+++ mhelp-vueadmin/mhelp-admin/src/views/Home.vue
@@ -1,31 +1,7 @@
 <template>
   <el-container>
     <el-aside width="200px">
-      <el-menu class="el-menu-vertical-demo" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
-        <router-link to="/index">
-          <el-menu-item index="Index">
-            <template slot="title"><i class="el-icon-s-home"></i> <span slot="title">首页</span></template>
-          </el-menu-item>
-        </router-link>
-        <el-submenu index="1">
-          <template slot="title"><i class="el-icon-s-operation"></i> <span>系统管理</span></template>
-          <el-menu-item index="1-1">
-            <template slot="title"><i class="el-icon-s-custom"></i> <span slot="title">用户管理</span></template>
-          </el-menu-item>
-          <el-menu-item index="1-2">
-            <template slot="title"><i class="el-icon-rank"></i> <span slot="title">角色管理</span></template>
-          </el-menu-item>
-          <el-menu-item index="1-3">
-            <template slot="title"><i class="el-icon-menu"></i> <span slot="title">菜单管理</span></template>
-          </el-menu-item>
-        </el-submenu>
-        <el-submenu index="2">
-          <template slot="title"><i class="el-icon-s-tools"></i> <span>系统工具</span></template>
-          <el-menu-item index="2-2">
-            <template slot="title"><i class="el-icon-s-order"></i> <span slot="title">数字字典</span></template>
-          </el-menu-item>
-        </el-submenu>
-      </el-menu>
+      <side-menu></side-menu>
     </el-aside>
 
     <el-container>
@@ -56,8 +32,10 @@
 </template>
 
 <script>
+import SideMenu from "./inc/sideMenu";
 export default {
   name: "Home.vue",
+  components: {SideMenu},
   data() {
     return {
       userInfo: {
@@ -99,10 +77,7 @@ export default {
   justify-content: space-around;
 
 }
-/*菜单栏*/
-.el-menu-vertical-demo{
-  height: 100%;
-}
+
 
 .el-header {
   background-color: #B3C0D1;
