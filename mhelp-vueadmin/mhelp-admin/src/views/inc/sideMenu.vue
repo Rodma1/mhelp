a8d79a75c11a632bc395bfb8f06378ea60e53b59
Rodma1 <1156028692@qq.com>
2021-11-29 17:38:24 +0800

    动态菜单栏开发一（管理员端）

--- mhelp-vueadmin/mhelp-admin/src/views/inc/sideMenu.vue
+++ mhelp-vueadmin/mhelp-admin/src/views/inc/sideMenu.vue
@@ -5,36 +5,46 @@
         <template slot="title"><i class="el-icon-s-home"></i> <span slot="title">首页</span></template>
       </el-menu-item>
     </router-link>
-    <el-submenu index="1">
-      <template slot="title"><i class="el-icon-s-operation"></i> <span>系统管理</span></template>
-      <el-menu-item index="1-1">
-        <template slot="title"><i class="el-icon-s-custom"></i> <span slot="title">用户管理</span></template>
-      </el-menu-item>
-      <el-menu-item index="1-2">
-        <template slot="title"><i class="el-icon-rank"></i> <span slot="title">角色管理</span></template>
-      </el-menu-item>
-      <el-menu-item index="1-3">
-        <template slot="title"><i class="el-icon-menu"></i> <span slot="title">菜单管理</span></template>
-      </el-menu-item>
+    <el-submenu :index="menu.name" v-for="menu in menuList">
+      <template slot="title"><i :class="menu.icon"></i> <span>{{ menu.title }}</span></template>
+      <!-- 子： 响应点击链接-->
+      <router-link :to="item.path" v-for="item in menu.children">
+        <el-menu-item :index="item.name">
+          <template slot="title"><i :class="item.icon"></i> <span slot="title">{{item.title}}</span></template>
+        </el-menu-item>
+      </router-link>
     </el-submenu>
-    <el-submenu index="2">
-      <template slot="title"><i class="el-icon-s-tools"></i> <span>系统工具</span></template>
-      <el-menu-item index="2-2">
-        <template slot="title"><i class="el-icon-s-order"></i> <span slot="title">数字字典</span></template>
-      </el-menu-item>
-    </el-submenu>
+
   </el-menu>
 </template>
 
 <script>
 export default {
-  name: "sideMenu"
+  name: "sideMenu",
+  data() {
+    return {
+      menuList: [{
+        name: 'SysManga',
+        title: '系统管理',
+        icon: 'el-icon-s-operation',
+        path: '',
+        component: '',
+        children: [{name: 'SysUser', title: '用户管理', icon: 'el-icon-s-custom', path: '/sys/users', children: []}]
+      }, {
+        name: 'SysTools',
+        title: '系统工具',
+        icon: 'el-icon-s-tools',
+        path: '',
+        children: [{name: 'SysDict', title: '数字字典', icon: 'el-icon-s-order', path: '/sys/dicts', children: []},]
+      }],
+    }
+  }
 }
 </script>
 
 <style scoped>
 /*菜单栏*/
-.el-menu-vertical-demo{
+.el-menu-vertical-demo {
   height: 100%;
 }
 </style>
